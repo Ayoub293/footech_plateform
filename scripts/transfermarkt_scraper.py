47446e35e18ab437991b60d8fb85f273c842b25b
Ayoub293 <ahmedabuayoub2@gmail.com>
2025-01-25 00:30:48 +0200

    try fix 4

--- scripts/transfermarkt_scraper.py
+++ scripts/transfermarkt_scraper.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
 import time
 from pymongo import MongoClient
